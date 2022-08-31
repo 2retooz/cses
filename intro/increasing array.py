e5519422d279c07c6bad2f329c42515cac85f896
2retooz <112514261+2retooz@users.noreply.github.com>
2022-09-01 02:36:02 +0300

    Update increasing array.py

--- intro/increasing array.py
+++ intro/increasing array.py
@@ -1,9 +1,9 @@
 n = int(input())
 array = list(map(int, input().split()))
-answer, t = 0, array[0]
+answer, max_ = 0, array[0]
 
 for i in array:
-    t = max(t, i)
-    answer += t - i
+    max_ = max(max_, i)
+    answer += max_ - i
 
 print(answer)
