c7dbb4b885f0f529c183e5b21d8f517c02cb3411
2retooz <112514261+2retooz@users.noreply.github.com>
2022-08-31 06:21:16 +0300

    Update missing number.py

--- intro/missing number.py
+++ intro/missing number.py
@@ -1,7 +1,7 @@
 n = int(input())
-string = map(int, input().split())
+seq = map(int, input().split())
 
-given_sum = sum(list(string))
+given_sum = sum(list(seq))
 total_sum = n * (n + 1) // 2
 
 print(total_sum - given_sum)
